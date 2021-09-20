4a9e4a110ada991887a8b63dc00f57681ca902b8
Maxwell McEwan <79493232+MaxwellMcEwan@users.noreply.github.com>
2021-09-20 10:59:12 -0500

    Program 1

---
+++ helloWorld.cpp
@@ -0,0 +1,16 @@
+//Author: Maxwell McEwan
+//Assignment Title: Hello World
+//Assignment Description: Print "Hello World !!" To Screen
+//Due Date: 8/25/2021
+//Date Created: 8/23/2021
+//Date Last Modified: 8/23/2021
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World !!" << endl;
+
+    return 0;
+}
